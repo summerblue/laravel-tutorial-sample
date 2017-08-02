9b71dd7ce1b06d0b647b417c68c5c0210d01f614
Summer <summer@yousails.com>
2017-08-02 02:56:20 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+      <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
+      <a href="https://yousails.com/">
+        优帆远扬 | 创造不息，交付不止
+      </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{ route('about') }}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
